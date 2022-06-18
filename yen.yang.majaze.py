361e16be2c42e0cd2a14207ceecc834f2a69f607
yen yang majaze <103533467+hacker112281@users.noreply.github.com>
2022-06-19 01:41:56 +0430

    Create yen.yang.majaze.py

---
+++ yen.yang.majaze.py
@@ -0,0 +1,23 @@
+from time import sleep
+from colorama import Fore, init
+import platform as pf
+import os
+import pyfiglet
+
+print('[ 1 ] ddos                    [ 2 ] XXS                         [ 3 ] SQL                       [ 4 ] admin panel                             [ 5 ] port scan') 
+
+
+if select == '1':
+    os.system('python ddos.py') 
+    
+if select == '2':
+    os.system('python3 XXS.py') 
+     
+if select == '3':
+    os.system('python3 SQL.py') 
+    
+if select == '4':
+    os.system('python3 admin_panel.py') 
+      
+if select == '5'
+    os.system('python3 port_scan.py') 
